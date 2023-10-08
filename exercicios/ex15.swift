bef58dbbba3c00c9013f6cd7e5a07300553b3de5
Jenifer <jeniferdutra2000@icloud.com>
2023-10-08 12:48:16 -0300

    Modificadores de Arrays

---
+++ exercicios/ex15.swift
@@ -0,0 +1,11 @@
+/*
+  Declare um array de números inteiros.
+  Adicione um novo número ao array.
+  Imprima cada elemento do array em uma linha.
+*/
+
+var numerosInteiros: [Int] = [5, 9, 50, 74, 65, 2, 84, 470]
+
+numerosInteiros.append(48)
+
+print(numerosInteiros[2])
