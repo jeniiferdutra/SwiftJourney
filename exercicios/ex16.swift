5dab0a324e3164bfd9e57d2668ae5661b912dc2a
Jenifer <jeniferdutra2000@icloud.com>
2023-10-14 18:59:45 -0300

    Variáveis

---
+++ exercicios/ex16.swift
@@ -0,0 +1,11 @@
+/*
+Declare três constantes para representar as dimensões de uma caixa (comprimento, largura e altura).
+Calcule e imprima o volume da caixa usando a fórmula volume = comprimento × largura × altura.
+*/
+
+let comprimento = 8
+let largura = 17
+let altura = 22
+let volume = comprimento * largura * altura
+
+print("O volume da caixa é \(volume)")
---
+++ exercicios/ex17.swift
@@ -0,0 +1,10 @@
+/*
+Declare duas variáveis para representar a idade de duas pessoas.
+Calcule e imprima a diferença de idade entre essas duas pessoas.
+*/
+
+let idade1 = 27
+let idade2 = 23
+let diferenca = idade1 - idade2
+
+print("A diferença de idade é de \(diferenca) anos")
