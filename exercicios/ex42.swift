08514e564953a8fa56c3d6cc7961dd17dda92e19
Jenifer <jeniferdutra2000@icloud.com>
2023-10-29 20:48:53 -0300

    Tuplas

---
+++ exercicios/ex42.swift
@@ -0,0 +1,10 @@
+/*Crie duas tuplas representando datas (dia, mês, ano) e compare se as duas datas são iguais.*/
+
+let opcao1 = (dia: 24, mes: "Janeiro", ano: 2000)
+let opcao2 = (dia: 24, mes: "Janeiro", ano: 2000)
+
+if opcao1 == opcao2 {
+    print("As datas são iguais")
+} else {
+    print("As datas não são iguais")
+}
