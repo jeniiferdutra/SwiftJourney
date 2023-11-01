c493770d9bf95fadf224b785529b6d3787585374
Jenifer <jeniferdutra2000@icloud.com>
2023-11-01 18:51:26 -0300

    Inverter string

---
+++ exercicios/ex45.swift
@@ -0,0 +1,9 @@
+/*Crie uma função para inverter uma string. Por exemplo, se a entrada for "hello", a saída será "olleh".*/
+
+func inversao(word: String) {
+    let palavrs = String(word.reversed())
+    print(palavrs)
+}
+
+let resultado = "hello"
+inversao(word: resultado)
