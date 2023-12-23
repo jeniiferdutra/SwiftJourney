0751ad07a6fe9bfbfafe6df53e01c05c0c54fc10
Jenifer <jeniferdutra2000@icloud.com>
2023-12-23 12:42:15 -0300

    Concatenação de arrays

---
+++ exercicios/ex95.swift
@@ -0,0 +1,12 @@
+/*Conversão de Tipos de Dados
+Declare uma variável com um número inteiro e outra variável com um número decimal. Converta o número inteiro em um decimal e o número decimal em um inteiro, e imprima os resultados.*/
+
+
+let numero1 = 5
+let numero2 = 7.6
+
+let numero1Decimal = Double(numero1)
+let numero2Inteiro = Int(numero2)
+
+print(numero1Decimal)
+print(numero2Inteiro)
---
+++ exercicios/ex96.swift
@@ -0,0 +1,9 @@
+/*Concatenação de Arrays
+Crie dois arrays de strings e concatene-os em um único array. Em seguida, imprima o array resultante.*/
+
+let array01 = ["Lana", "Verde", "Vermelho"]
+let array02 = ["Cherry", "Preto", "Art"]
+
+let resultado = array01 + array02
+
+print(resultado)
