02b491007a8dd4591f6b123978f69368d34bb352
Jenifer <jeniferdutra2000@icloud.com>
2023-12-23 12:02:33 -0300

    Verificação de números pares e impares

---
+++ exercicios/ex94.swift
@@ -0,0 +1,10 @@
+/*Verificação de Números Pares e Ímpares
+Escreva um programa que verifique se um número é par ou ímpar e imprima o resultado.*/
+
+let numero = 11
+
+if numero % 2 == 0 {
+    print("Par")
+} else {
+    print("Impar")
+}
