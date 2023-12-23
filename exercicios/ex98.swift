cc45ba4cea7cd53bc9522e5718f0e233bf4badd1
Jenifer <jeniferdutra2000@icloud.com>
2023-12-23 13:02:14 -0300

    Incremento e decremento

---
+++ exercicios/ex98.swift
@@ -0,0 +1,11 @@
+/*Incremento e Decremento
+Crie uma variável numérica e utilize operadores de incremento e decremento para aumentar e diminuir seu valor. Imprima os resultados em cada etapa.*/
+
+var numero = 5
+print(numero)
+
+numero += 1
+print(numero)
+
+numero -= 1
+print(numero)
