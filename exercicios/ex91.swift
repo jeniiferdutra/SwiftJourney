9f7a0cd8fc0d48c16b58ee30b39f13e75241deed
Jenifer <jeniferdutra2000@icloud.com>
2023-12-23 11:13:51 -0300

    Concatenação de Strings

---
+++ exercicios/ex91.swift
@@ -0,0 +1,8 @@
+/*Concatenação de Strings
+Crie uma variável que contenha o seu nome e outra variável com a sua idade. Em seguida, imprima uma mensagem concatenando essas variáveis para exibir seu nome e idade juntos.*/
+
+let nome = "Jenifer"
+let idade = 23
+
+let resultado = nome + " tem " + String(idade) + " anos"
+print(resultado)
