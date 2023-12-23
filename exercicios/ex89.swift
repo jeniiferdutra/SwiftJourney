6998a4bb71e4a77eeb2daf2dc15eff49d21e00be
Jenifer <jeniferdutra2000@icloud.com>
2023-12-23 10:38:00 -0300

    Troca de valores

---
+++ exercicios/ex89.swift
@@ -0,0 +1,14 @@
+//Troca de Valores Crie um programa que troque os valores de duas variáveis. Por exemplo, se tivermos var a = 5 e var b = 10, o programa deve trocar os valores para que a seja igual a 10 e b seja igual a 5.//
+
+var a = 5
+var b = 10
+
+print(a)
+print(b)
+
+var novaA = b
+var novaB = a
+
+print("Resultado")
+print(novaA)
+print(novaB)
