f4329cc0942310f6454bd995970fefff41dd180e
Jenifer <jeniferdutra2000@icloud.com>
2023-12-23 11:59:18 -0300

    Inversão de string

---
+++ exercicios/ex93.swift
@@ -0,0 +1,7 @@
+/*Inversão de String
+Escreva um programa que pegue uma string e a imprima invertida. Por exemplo, se a string for "Swift", o programa deve imprimir "tfiwS"*/
+
+let palavra = "Swift"
+let palavraInvertida = String(palavra.reversed())
+
+print(palavraInvertida)
