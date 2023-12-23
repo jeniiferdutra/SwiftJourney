a2320f71d54e0cc2f637c2a389914437f073b125
Jenifer <jeniferdutra2000@icloud.com>
2023-12-23 12:57:28 -0300

    Comparação de strings

---
+++ exercicios/ex97.swift
@@ -0,0 +1,11 @@
+/*Comparação de Strings
+Declare duas strings diferentes e verifique se elas são iguais. Em seguida, imprima o resultado da comparação.*/
+
+let teste1 = "Jenifer"
+let teste2 = "Daniel"
+
+if teste1 == teste2 {
+    print("São iguais")
+} else {
+    print("Strings diferentes")
+}
