a3daa4123494afb3a38cfa03af0dd1239ddbb953
Jenifer <jeniferdutra2000@icloud.com>
2023-12-23 10:41:29 -0300

    Calculo de valores

---
+++ exercicios/ex90.swift
@@ -0,0 +1,10 @@
+/*Cálculo de Média
+Escreva um programa que calcule a média de três números e imprima o resultado.*/
+
+let numero1 = 5
+let numero2 = 6
+let numero3 = 9
+
+let media = (numero1 + numero2 + numero3) / 3
+
+print("O resultado da média dos números é \(media)")
