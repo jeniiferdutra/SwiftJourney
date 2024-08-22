d62c83b8f73ac1615851140867edd8d1145fa256
jenifer rocha <jeniferdutra2000@icloud.com>
2024-08-22 19:46:41 -0300

    converter valores

---
+++ primeirosPassos/converter-valores/converter-valores/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  converter-valores
+//
+//  Created by Jenifer Rocha on 22/08/24.
+//
+
+import Foundation
+
+let x = 20
+let name = String(x)
+print(type(of: name))
+
+let y = 10
+let value = Int(y)
+print(type(of: value))
+
+var frase01 = "Bom dia"
+var frase02 = "Boa noite"
+
+(frase01, frase02) = (frase02, frase01)
+
+print(frase01)
+print(frase02)
